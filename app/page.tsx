b38f8e6955bec59be1c96599054073eb35a8267a
Khalid <olusesikhalid43@gmail.com>
2026-07-13 09:24:07 +0100

    playing around to refresh my brain

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-export default function HomePage () {
+export default function HomePage() {
   return (
-    <h1 className="text-2xl text-emerald-400 font-bold">I am currently in the homepage</h1>
-  )
+    <div className="flex justify-center items-center border-2 m-3 rounded-2xl">
+      <h1 className="text-2xl text-emerald-400 font-bold">
+        I am currently in the homepage
+      </h1>
+    </div>
+  );
 }
